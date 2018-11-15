630513f4e080938932751917b95bbadbc07b3a26
Jeffrey Cherewaty <jeffrey@cherewaty.com>
2018-11-15 14:20:59 -0600

    Bobby and Bobbie Begat Bob

--- tests/factory-test.js
+++ tests/factory-test.js
@@ -17,59 +17,59 @@ describe('Factories', () => {
 
   describe('creating with static defaults', () => {
     beforeEach(() => {
-      let StoreType = Store(Person, (attrs) => ({ name: 'Bob' }));
-      people = create(StoreType).create().create();
+      let People = Store(Person, (attrs) => ({ name: 'Bob' }));
+      people = create(People).createRecord().createRecord();
     });
 
     it('adds a person to the people store with the default attributes', () => {
-      expect(people.entities[0].name.state).toEqual('Bob');
-      expect(people.entities[1].name.state).toEqual('Bob');
+      expect(people.records[0].name.state).toEqual('Bob');
+      expect(people.records[1].name.state).toEqual('Bob');
     });
   });
 
   describe('creating with higher order defaults', () => {
     beforeEach(() => {
-      let StoreType = Store(Person, (attrs) => ({
+      let People = Store(Person, (attrs) => ({
         name: (id) => id % 2 ? 'Bob' : 'Bill'
       }));
 
-      people = create(StoreType).create().create();
+      people = create(People).createRecord().createRecord();
     });
 
     it('adds a person to the people store with the default attributes', () => {
-      expect(people.entities[0].name.state).toEqual('Bill');
-      expect(people.entities[1].name.state).toEqual('Bob');
+      expect(people.records[0].name.state).toEqual('Bill');
+      expect(people.records[1].name.state).toEqual('Bob');
     });
   });
 
   describe('creating with overrides', () => {
     beforeEach(() => {
-      let StoreType = Store(Person, (attrs) => ({ name: 'Bob' }));
-      people = create(StoreType).create({ name: 'Wil' });
+      let People = Store(Person, (attrs) => ({ name: 'Bob' }));
+      people = create(People).createRecord({ name: 'Wil' });
     });
 
     it('adds a person to the people store with overridden attributes', () => {
-      expect(people.entities[0].name.state).toEqual('Wil');
+      expect(people.records[0].name.state).toEqual('Wil');
     });
   });
 
-  describe('creating multiple entities at once', () => {
+  describe('creating multiple records at once', () => {
     beforeEach(() => {
-      let StoreType = Store(Person, (attrs) => ({ name: (id) => `Bob ${id}` }));
-      people = create(StoreType).createMany(5);
+      let People = Store(Person, (attrs) => ({ name: (id) => `Bob ${id}` }));
+      people = create(People).createRecords(5);
     });
 
     it('adds multiple people to the store', () => {
-      // entities object does not have enumerable keys
-      expect(getOwnPropertyNames(people.entities).length).toEqual(5);
+      // records object does not have enumerable keys
+      expect(getOwnPropertyNames(people.records).length).toEqual(5);
     });
 
     it('dynamically generates attributes for each person', () => {
-      expect(people.entities[0].name.state).toEqual('Bob 0');
-      expect(people.entities[1].name.state).toEqual('Bob 1');
-      expect(people.entities[2].name.state).toEqual('Bob 2');
-      expect(people.entities[3].name.state).toEqual('Bob 3');
-      expect(people.entities[4].name.state).toEqual('Bob 4');
+      expect(people.records[0].name.state).toEqual('Bob 0');
+      expect(people.records[1].name.state).toEqual('Bob 1');
+      expect(people.records[2].name.state).toEqual('Bob 2');
+      expect(people.records[3].name.state).toEqual('Bob 3');
+      expect(people.records[4].name.state).toEqual('Bob 4');
     });
   });
 
@@ -82,24 +82,43 @@ describe('Factories', () => {
       fatherId = String;
 
       get mother() {
-        return people(this).entities[this.motherId.state];
+        return people(this).records[this.motherId.state];
       }
 
       get father() {
-        return people(this).entities[this.fatherId.state];
+        return people(this).records[this.fatherId.state];
       }
     }
 
     beforeEach(() => {
-      let StoreType = Store(Person, (attrs) => ({ name: `Bob` }));
-      people = create(StoreType)
-        .create();
+      let People = Store(Person, (attrs) => ({ name: `Bob` }));
+      people = create(People);
     });
 
-    it('adds a person with relationships', () => {
+    describe('creating with null relationships', () => {
+      beforeEach(() => {
+        people = people.createRecord();
+      });
 
+      it('adds a person without parents', () => {
+        expect(people.records[0].mother).toEqual(null);
+        expect(people.records[0].father).toEqual(null);
+      });
+    });
+
+    describe('creating with relationships' , () => {
+      beforeEach(() => {
+        people = people.createRecord({
+          mother: { name: 'Bobbie' },
+          father: { name: 'Bobby' }
+        });
+      });
+
+      it('adds a person with parents', () => {
+        expect(people.records[0].mother.name.state).toEqual('Bob');
+        expect(people.records[0].father.name.state).toEqual('Bob');
+      });
     });
   });
 
-  describe.skip('creating with overridden relationships');
 });
