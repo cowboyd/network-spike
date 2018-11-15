842f8d2b2ca806a9741eae34dec508a36d9b3792
David Keathley <david@frontside.io>
2018-11-15 12:40:05 -0600

    Start working on factories

---
+++ tests/factory-test.js
@@ -0,0 +1,22 @@
+import { Store } from '../src/store';
+import Factory from '../src/factory';
+
+describe('Factories', () => {
+  describe('creating with defaults', () => {
+    beforeEach(() => {
+
+    });
+    
+    it('works', () => {
+
+    });
+  });
+
+  describe.skip('creating with overrides');
+
+  describe.skip('creating multiple entities at once');
+
+  describe.skip('creating with relationships');
+
+  describe.skip('creating with overridden relationships');
+});
